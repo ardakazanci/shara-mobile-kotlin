5327932a0df868afeabc76163322beba4f3823cd
Juma Allan <allanjuma@gmail.com>
2020-05-21 00:15:25 +0300

    Setting up Retrofit API

---
+++ app/src/main/java/co/shara/data/retrofit/UserAPI.kt
@@ -0,0 +1,15 @@
+package co.shara.data.retrofit
+
+import co.shara.data.model.User
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserAPI {
+
+    @POST("/api/v1/user/register")
+    suspend fun registerUser(@Body userValidation: User): User?
+
+    @POST("/api/v1/user/login")
+    suspend fun loginUser(@Body userLogin: User): User?
+
+}
