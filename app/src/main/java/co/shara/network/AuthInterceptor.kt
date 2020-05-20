47b794dd9c17addefef6f39fb7c9ea8b8453f162
Juma Allan <allanjuma@gmail.com>
2020-05-21 00:00:21 +0300

    setting up auth interceptor

---
+++ app/src/main/java/co/shara/network/AuthInterceptor.kt
@@ -0,0 +1,24 @@
+package co.shara.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(
+    private val settings: Settings,
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var chainRequest = chain.request()
+        chainRequest = if (chainRequest.headers.names().contains("No-Auth")) {
+            chainRequest.newBuilder().removeHeader("No-Auth").build()
+        } else {
+            chainRequest.newBuilder()
+                .addHeader(
+                    "Authorization",
+                    "Bearer " + settings.getBearerToken().orEmpty()
+                )
+                .build()
+        }
+        return chain.proceed(chainRequest)
+    }
+}
