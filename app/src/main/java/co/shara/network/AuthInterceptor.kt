0ab5861c656b5809cfa663755a728f832f6bd196
Juma Allan <allanjuma@gmail.com>
2020-05-21 00:01:53 +0300

    setting up shared pref file

--- app/src/main/java/co/shara/network/AuthInterceptor.kt
+++ app/src/main/java/co/shara/network/AuthInterceptor.kt
@@ -1,5 +1,6 @@
 package co.shara.network
 
+import co.shara.settings.Settings
 import okhttp3.Interceptor
 import okhttp3.Response
 
---
+++ app/src/main/java/co/shara/settings/Settings.kt
@@ -0,0 +1,50 @@
+package co.shara.settings
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+
+class Settings(
+    private val settings: SharedPreferences
+) {
+
+    fun getBearerToken(): String? {
+        return settings.getString(SettingsConstants.BEARER_TOKEN_KEY, "")
+    }
+
+    fun setBearerToken(bearer_token: String) {
+        settings.edit {
+            putString(SettingsConstants.BEARER_TOKEN_KEY, bearer_token)
+        }
+    }
+
+    fun getUserId(): String? {
+        return settings.getString(SettingsConstants.USER_ID_KEY, "")
+    }
+
+    fun setUserId(user_id: String) {
+        settings.edit {
+            putString(SettingsConstants.USER_ID_KEY, user_id)
+        }
+    }
+
+    fun isLoggedIn(): Boolean {
+        return settings.getBoolean(SettingsConstants.LOGGED_IN_KEY, false)
+    }
+
+    fun setIsLoggedIn(is_logged_in: Boolean) {
+        settings.edit {
+            putBoolean(SettingsConstants.LOGGED_IN_KEY, is_logged_in)
+        }
+    }
+
+    fun clearData() {
+        settings.edit { clear() }
+    }
+}
+
+object SettingsConstants {
+    const val BEARER_TOKEN_KEY = "bearer_token"
+    const val USER_ID_KEY = "user_id"
+    const val LOGGED_IN_KEY = "is_logged_in"
+}
