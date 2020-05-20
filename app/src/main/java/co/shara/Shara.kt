e58e6da4de55b2388be87b21104f999c8a89283b
Juma Allan <allanjuma@gmail.com>
2020-05-21 00:04:51 +0300

    setting up shara application class

---
+++ app/src/main/java/co/shara/Shara.kt
@@ -0,0 +1,43 @@
+package co.shara
+
+import android.app.Application
+import androidx.annotation.Nullable
+import org.jetbrains.annotations.NotNull
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import timber.log.Timber
+
+class Shara : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+        initKoin()
+//        injectFeature()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+    }
+
+    private fun initKoin() {
+        try {
+            startKoin {
+                androidLogger()
+                androidContext(applicationContext)
+            }
+        } catch (error: KoinAppAlreadyStartedException) {
+            Timber.e(error.localizedMessage)
+        }
+    }
+}
