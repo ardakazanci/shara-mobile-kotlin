78225572722b7092a6c852667e82712429362fa5
Juma Allan <allanjuma@gmail.com>
2020-05-21 00:33:42 +0300

    setting up auth on view model

--- app/src/main/java/co/shara/ui/viewmodel/UserViewModel.kt
+++ app/src/main/java/co/shara/ui/viewmodel/UserViewModel.kt
@@ -3,10 +3,21 @@ package co.shara.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import co.shara.data.model.User
 import co.shara.data.repo.UserRepository
+import co.shara.data.retrofit.UserLogin
+import co.shara.data.retrofit.UserRegister
+import co.shara.network.NetworkResult
 
 class UserViewModel(
     private val userRepository: UserRepository
 ) : ViewModel() {
+
+    suspend fun registerUser(userRegister: UserRegister): NetworkResult<User> {
+        return userRepository.registerUser(userRegister)
+    }
+
+    suspend fun loginUser(userLogin: UserLogin): NetworkResult<User> {
+        return userRepository.loginUser(userLogin)
+    }
 
     suspend fun saveUser(user: User) {
         return userRepository.saveUser(user)
