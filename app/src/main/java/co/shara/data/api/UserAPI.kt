9fa2b6f87c224e4928e8271cb166f04516986f99
Juma Allan <allanjuma@gmail.com>
2020-05-21 00:32:03 +0300

    Setting up the register and login

--- app/src/main/java/co/shara/data/api/UserAPI.kt
+++ app/src/main/java/co/shara/data/api/UserAPI.kt
@@ -1,15 +1,17 @@
 package co.shara.data.api
 
-import co.shara.data.model.User
+import co.shara.data.retrofit.UserLogin
+import co.shara.data.retrofit.UserRegister
+import co.shara.data.retrofit.UserResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface UserAPI {
 
     @POST("/api/v1/user/register")
-    suspend fun registerUser(@Body userValidation: User): User?
+    suspend fun registerUser(@Body userRegister: UserRegister): UserResponse?
 
     @POST("/api/v1/user/login")
-    suspend fun loginUser(@Body userLogin: User): User?
+    suspend fun loginUser(@Body userLogin: UserLogin): UserResponse?
 
 }
--- app/src/main/java/co/shara/data/repo/UserRepository.kt
+++ app/src/main/java/co/shara/data/repo/UserRepository.kt
@@ -3,13 +3,58 @@ package co.shara.data.repo
 import co.shara.data.api.UserAPI
 import co.shara.data.dao.UserDao
 import co.shara.data.model.User
+import co.shara.data.retrofit.UserLogin
+import co.shara.data.retrofit.UserRegister
+import co.shara.network.NetworkResult
+import co.shara.network.safeApiCall
 import co.shara.settings.Settings
+import kotlinx.coroutines.Dispatchers
 
 class UserRepository(
     private val userDao: UserDao,
     private val userAPI: UserAPI,
     private val settings: Settings
 ) {
+
+    suspend fun registerUser(userRegister: UserRegister): NetworkResult<User> {
+        return safeApiCall(Dispatchers.IO) {
+            val loginResponse = userAPI.registerUser(userRegister)
+            val user = User(
+                0,
+                loginResponse?.user_id,
+                loginResponse?.first_name,
+                loginResponse?.last_name,
+                loginResponse?.phone_number,
+                loginResponse?.email
+            )
+
+            // update the shared pref here
+            settings.setUserId(loginResponse?.user_id.toString())
+//            settings.setBearerToken(loginResponse?.jwt_token.orEmpty())
+
+            user.copy()
+        }
+    }
+
+    suspend fun loginUser(userLogin: UserLogin): NetworkResult<User> {
+        return safeApiCall(Dispatchers.IO) {
+            val loginResponse = userAPI.loginUser(userLogin)
+            val user = User(
+                0,
+                loginResponse?.user_id,
+                loginResponse?.first_name,
+                loginResponse?.last_name,
+                loginResponse?.phone_number,
+                loginResponse?.email
+            )
+
+            // update the shared pref here
+            settings.setUserId(loginResponse?.user_id.toString())
+//            settings.setBearerToken(loginResponse?.jwt_token.orEmpty())
+
+            user.copy()
+        }
+    }
 
     suspend fun saveUser(user: User) {
         userDao.insert(user)
