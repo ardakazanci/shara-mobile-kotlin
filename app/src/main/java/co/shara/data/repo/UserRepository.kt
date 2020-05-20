2aaa0d2219a8d1de5c78bb1eb7adf334fe8323e0
Juma Allan <allanjuma@gmail.com>
2020-05-21 00:15:44 +0300

    Setting up User Repo

---
+++ app/src/main/java/co/shara/data/repo/UserRepository.kt
@@ -0,0 +1,21 @@
+package co.shara.data.repo
+
+import co.shara.data.dao.UserDao
+import co.shara.data.model.User
+import co.shara.data.retrofit.UserAPI
+import co.shara.settings.Settings
+
+class UserRepository(
+    private val userDao: UserDao,
+    private val userAPI: UserAPI,
+    private val settings: Settings
+) {
+
+    suspend fun saveUser(user: User) {
+        userDao.insert(user)
+    }
+
+    suspend fun getUserById(userId: String): User {
+        return userDao.getUserById(userId)
+    }
+}
