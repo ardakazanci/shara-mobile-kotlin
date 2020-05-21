967f5f116b16baa6f56f5f7a6844596024e2d6fa
Juma Allan <allanjuma@gmail.com>
2020-05-21 12:06:06 +0300

    setting up order products

---
+++ app/src/main/java/co/shara/ui/views/OrderProductActivity.kt
@@ -0,0 +1,13 @@
+package co.shara.ui.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.shara.R
+
+class OrderProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_product)
+    }
+}
