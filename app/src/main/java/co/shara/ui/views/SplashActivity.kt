adbde6e48095214ffdbeac4353f3fe1f61edf13f
Juma Allan <allanjuma@gmail.com>
2020-05-21 12:26:24 +0300

    Setting up Shara splash

--- app/src/main/java/co/shara/ui/views/SplashActivity.kt
+++ app/src/main/java/co/shara/ui/views/SplashActivity.kt
@@ -1,13 +1,23 @@
 package co.shara.ui.views
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import co.shara.R
+import co.shara.util.makeStatusBarTransparent
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_splash)
+
+        makeStatusBarTransparent()
     }
 }
