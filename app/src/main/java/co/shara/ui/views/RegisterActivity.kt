d7304f673f0ea7c14cb91c349ac9542a9b5140ba
Juma Allan <allanjuma@gmail.com>
2020-05-21 12:05:22 +0300

    setting up auth - login and register

---
+++ app/src/main/java/co/shara/ui/views/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package co.shara.ui.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.shara.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
