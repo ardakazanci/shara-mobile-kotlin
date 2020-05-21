0ed943d94feff8364a8a92c7d0cef460657b8afe
Juma Allan <allanjuma@gmail.com>
2020-05-21 12:38:59 +0300

    underline span setup

--- app/src/main/java/co/shara/util/Util.kt
+++ app/src/main/java/co/shara/util/Util.kt
@@ -1,8 +1,27 @@
 package co.shara.util
 
+import android.content.Context
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.UnderlineSpan
+import androidx.core.content.res.ResourcesCompat
 import java.util.*
 
 object Util {
+
+    fun setUnderlinedSpan(
+        context: Context,
+        text: String,
+        styledText: String
+    ): CharSequence? {
+        val content = SpannableString(text)
+        val start = text.indexOf(styledText)
+        val end = start + styledText.length
+        val textColor = ResourcesCompat.getColor(context.resources, R.color.colorPrimary, null)
+        content.setSpan(UnderlineSpan(), start, end, 0)
+        content.setSpan(ForegroundColorSpan(textColor), start, end, 0)
+        return content
+    }
 
     fun getCurrentDateTime(): Date {
         return Calendar.getInstance().time
