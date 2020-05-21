8c7b991b6352c4e4bc5a25240ff7427b4ae642b9
Juma Allan <allanjuma@gmail.com>
2020-05-21 12:01:06 +0300

    Refactored OrderActivity

--- app/src/main/java/co/shara/ui/views/OrderActivity.kt
+++ app/src/main/java/co/shara/ui/views/OrderActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import co.shara.R
 
-class MainActivity : AppCompatActivity() {
+class OrderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_order)
     }
 }
