d465f06c17c6b6095d7b561e2aeb26f5c0b2187c
Juma Allan <allanjuma@gmail.com>
2020-05-21 13:50:22 +0300

    setting up register <> heroku

--- app/src/main/java/co/shara/data/retrofit/UserRegister.kt
+++ app/src/main/java/co/shara/data/retrofit/UserRegister.kt
@@ -1,8 +1,7 @@
 package co.shara.data.retrofit
 
 data class UserRegister(
-    val first_name: String?,
-    val last_name: String?,
+    val name: String?,
     val phone_number: String?,
     val email: String?,
     var password: String
--- app/src/main/java/co/shara/ui/views/RegisterActivity.kt
+++ app/src/main/java/co/shara/ui/views/RegisterActivity.kt
@@ -6,16 +6,24 @@ import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import co.shara.R
+import co.shara.data.retrofit.UserRegister
+import co.shara.network.NetworkResult
+import co.shara.settings.Settings
 import co.shara.ui.viewmodel.UserViewModel
 import co.shara.util.Util
+import co.shara.util.makeSnackbar
 import co.shara.util.makeStatusBarTransparent
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RegisterActivity : AppCompatActivity() {
 
     private val userViewModel: UserViewModel by viewModel()
+    private val settings: Settings by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,8 +44,73 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 
+    fun validateUser(view: View) {
+
+        val name = etName.text.toString().trim()
+        val email = etEmail.text.toString().trim()
+        val phoneNumber = etPhoneNumber.text.toString().trim()
+        val password = etPin.text.toString().trim()
+
+        if (name.isEmpty()) {
+            etName.error = "Name is required"
+            etName.requestFocus()
+            return
+        }
+
+        if (email.isEmpty()) {
+            etEmail.error = "Email is required"
+            etEmail.requestFocus()
+            return
+        }
+
+        if (phoneNumber.isEmpty()) {
+            etPhoneNumber.error = "Phone number is required"
+            etPhoneNumber.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            etPin.error = "Password is required"
+            etPin.requestFocus()
+            return
+        }
+
+        registerUser(name, email, phoneNumber, password)
+    }
+
+    private fun registerUser(name: String, email: String, phoneNumber: String, password: String) {
+
+//        val progressDialog = makeProgressDialog("Signing In...")
+//        progressDialog.show()
+
+        lifecycleScope.launch {
+            when (val loginResult =
+                userViewModel.registerUser(UserRegister(name, email, phoneNumber, password))) {
+                is NetworkResult.Success -> {
+
+                    userViewModel.saveUser(loginResult.data)
+
+                    // update the shared pref here
+                    settings.setIsLoggedIn(true)
+
+                    makeSnackbar("user registration is successful")
+                }
+                is NetworkResult.ServerError -> {
+                    makeSnackbar(loginResult.errorBody?.message ?: "Failed to register user.")
+                }
+                NetworkResult.NetworkError -> {
+                    makeSnackbar("A network error occurred. Please try again later.")
+                }
+                NetworkResult.Loading -> {
+                }
+            }
+        }
+
+    }
+
     fun login(view: View) {
         val intent = Intent(applicationContext, LoginActivity::class.java)
         startActivity(intent)
     }
+
 }
