3564d0491a4828ea80277fcf34a9d4d9405339f2
Juma Allan <allanjuma@gmail.com>
2020-05-22 12:30:51 +0300

    inject order viewmodel and shared pref

--- app/src/main/java/co/shara/ui/views/OrderProductActivity.kt
+++ app/src/main/java/co/shara/ui/views/OrderProductActivity.kt
@@ -3,8 +3,15 @@ package co.shara.ui.views
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import co.shara.R
+import co.shara.settings.Settings
+import co.shara.ui.viewmodel.OrderViewModel
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OrderProductActivity : AppCompatActivity() {
+
+    private val orderViewModel: OrderViewModel by viewModel()
+    private val settings: Settings by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
