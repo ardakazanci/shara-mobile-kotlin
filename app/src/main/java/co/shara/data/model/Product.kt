b75b98dff6b0c53aa18b0f6f9dd8c1f10973a3bd
Juma Allan <allanjuma@gmail.com>
2020-05-27 02:23:30 +0300

    updated product model

--- app/src/main/java/co/shara/data/model/Product.kt
+++ app/src/main/java/co/shara/data/model/Product.kt
@@ -7,5 +7,9 @@ import androidx.room.PrimaryKey
 data class Product(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
-    val product_id: Int?
+    val product_id: Int,
+    var order_id: Int,
+    var name: String,
+    var price: String,
+    var uom: String
 )
