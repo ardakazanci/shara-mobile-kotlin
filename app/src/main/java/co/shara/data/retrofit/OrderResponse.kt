c945fbccbcdd0669c044ee5a6490d98790c7a13f
Juma Allan <allanjuma@gmail.com>
2020-05-27 02:24:21 +0300

    update order response >> attach user id

--- app/src/main/java/co/shara/data/retrofit/OrderResponse.kt
+++ app/src/main/java/co/shara/data/retrofit/OrderResponse.kt
@@ -2,6 +2,7 @@ package co.shara.data.retrofit
 
 data class OrderResponse(
     var order_id: Int,
+    var user_id: Int,
     var order_number: String,
     var order_products: List<ProductsResponse>
 )
